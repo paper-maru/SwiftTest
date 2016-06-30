45e05e19db8238c805342c66657cb648fe7ef23c
maru <maruyama@paper-less.jp>
2016-06-30 15:28:26 +0900

    add
    add

--- SwiftTest/ViewController.swift
+++ SwiftTest/ViewController.swift
@@ -13,6 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let label = UILabel.init(frame: CGRectMake(self.view.frame.size.width/2, self.view.frame.size.height/2, 100, 100))
+        
+        label.backgroundColor = UIColor.blackColor()
+        
+        label.textColor = UIColor.whiteColor()
+        
+        label.text = "Hello"
+        
+        label.textAlignment = .Center
+        
+        label.font = UIFont.systemFontOfSize(15)
+        
+        self.view.addSubview(label)
     }
 
     override func didReceiveMemoryWarning() {
